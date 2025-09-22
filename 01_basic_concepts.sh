92124ba7a655197b35905e984ca10e0c0bcb127a
jesus.loport <jesus.loport@outlook.com>
2025-09-22 12:41:10 -0600

    exercise 01: bash scripts & autograde fix

---
+++ 01_basic_concepts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ┌────────────────────────────────────────┐
+# │ Script Bash para ejercicio             │
+# └────────────────────────────────────────┘
+
+# Ejecutar el módulo de Python
+python3 -m exercises.01_basic_concepts.app
